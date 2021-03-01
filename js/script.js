8de088386385a22c7199f38d99ae2fb1e2ae328f
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-03-01 18:47:01 +0100

    Parte Logica aggiunto bonus difficoltà

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*  Il computer deve generare 16 numeri casuali tra 1 e 100.
 I numeri non possono essere duplicati
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+In seguito deve chiedere all’utente (100- 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 L’utente non può inserire più volte lo stesso numero.
 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
@@ -11,10 +11,12 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50 */
 
+// Funzione numeri random
 function rndNum(min, max) {
     return Math.floor(Math.random() * (max- min + 1) + min);
 } 
 
+// Funzione controllo duplicato utente
 function isDuplicate(array, x) {
     var bool = false;
     for (var i = 0; i < array.length; i++) {
@@ -25,7 +27,8 @@ function isDuplicate(array, x) {
     return bool;
 }
 
-function removeArrayDuplicate(array) {
+// Funzione rimozione duplicati bombe
+function removeArrayDuplicate(array, difficulty) {
     //ordinamento array
     array = array.sort(function(a, b){
         return a - b;
@@ -33,7 +36,7 @@ function removeArrayDuplicate(array) {
 
     for (var i = 0, j = 1; i < array.length; i++, j++) {
         if (array[i] == array[j]) {
-            array[j] = (rndNum(1, 100));
+            array[j] = (rndNum(1, difficulty));
             array = array.sort(function (a, b) {
                 return a - b;
             });
@@ -44,43 +47,56 @@ function removeArrayDuplicate(array) {
     return array;
 }
 
-function generateBombs() {
+// Funzione genera bombe
+function generateBombs(difficulty) {
     var bombArray = [];
     var bombArrayLength = 16;
     for (var i = 0; i < bombArrayLength; i++) {
-        bombArray.push(rndNum(1, 100));
+        bombArray.push(rndNum(1, difficulty));
     }
     return bombArray;
 }
 
-var bombArray = generateBombs();
-console.log(bombArray);
+var submit = document.getElementById('submit');
 
-var bombArrayNoDup = removeArrayDuplicate(bombArray);
-console.log(bombArrayNoDup);
+submit.addEventListener('click', 
+    function() {
+        // variabili
+        var difficulty = parseInt(document.getElementById('ms_game_difficulty').value);
+        console.log(difficulty);
 
-var userArray = [];
-const userArrayLength = 100 - 16;
-var counter = -1;
-var topscore = 0;
+        var bombArray = generateBombs(difficulty);
+        console.log(bombArray);
 
-do {
-    var userNum = parseInt(prompt('Inserisci un numero tra 1 e 100'));
-    while (userNum <= 0 || userNum > 100 || isNaN(userNum)) {
-        alert('Inserisci un numero tra 1 e 100');
-        userNum = parseInt(prompt('Inserisci un numero tra 1 e 100'));
+        var bombArrayNoDup = removeArrayDuplicate(bombArray, difficulty);
+        console.log(bombArrayNoDup);
+
+        var userArray = [];
+        const userArrayLength = difficulty - 16;
+        var counter = -1;
+        var topscore = 0;
+
+        // inserimento numeri utente
+        do {
+            var userNum = parseInt(prompt('Inserisci un numero tra 1 e ' + difficulty));
+            while (userNum <= 0 || userNum > difficulty || isNaN(userNum)) {
+                alert('Inserisci un numero tra 1 e difficulty');
+                userNum = parseInt(prompt('Inserisci un numero tra 1 e ' + difficulty));
+            }
+            while (isDuplicate(userArray, userNum)) {
+                alert('Hai già inserito questo numero, ritenta con un altro');
+                userNum = parseInt(prompt('Inserisci un numero tra 1 e ' + difficulty));
+            }
+            userArray.push(userNum);
+            counter += 1;
+        } while ((userArray.length < userArrayLength) && (isDuplicate(bombArray, userNum) == false));
+
+        console.log(userNum);
+        console.log(userArray);
+        console.log(isDuplicate(bombArray, userNum));
+        console.log("hai fatto " + counter);
+
+        // stampa punteggio
+        document.getElementById('ms_counter').innerHTML = "Hai fatto: " + counter;
     }
-    while (isDuplicate(userArray, userNum)) {
-        alert('Hai già inserito questo numero, ritenta con un altro');
-        userNum = parseInt(prompt('Inserisci un numero tra 1 e 100'));
-    }
-    userArray.push(userNum);
-    counter += 1;
-} while ((userArray.length < userArrayLength) && (isDuplicate(bombArray, userNum) == false));
-
-console.log(userNum);
-console.log(userArray);
-console.log(isDuplicate(bombArray, userNum));
-console.log("hai fatto " + counter);
-
-document.getElementById('ms_counter').innerHTML = "Hai fatto: " + counter;
+);
